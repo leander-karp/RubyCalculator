0588c0fdb733a046b45900ee86c5ed0b9f2b5f77
Leander Karp <leander.karp@invision.de>
2020-11-06 11:47:23 +0100

    initial commit

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,11 @@
+require 'calculator'
+
+RSpec.describe Calculator do
+  describe '#solve' do
+    it 'handles empty input correctly'
+    it 'ingores whitespaces'
+    it 'can read integers'
+    it 'can read floats'
+    it 'can add two numbers'
+  end
+end
