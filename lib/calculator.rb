4d6d41988e3ac51f595eab7bcbc59bdcb01a31f4
Leander Karp <leander.karp@invision.de>
2020-11-06 13:08:58 +0100

    Add + Operator support

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,1 +1,23 @@
-class Calculator; end
+# frozen_string_literal: true
+
+# The Calculator class can be initialized with a string and solve the expression within.
+class Calculator
+  def initialize(expression)
+    @expression = expression
+  end
+
+  def solve
+    if expression.include?('+')
+      first_operand, second_operand = expression.split('+')
+      first_operand.strip!
+      second_operand.strip!
+      (first_operand.to_i + second_operand.to_i).to_s
+    else
+      expression.strip
+    end
+  end
+
+  private
+
+  attr_reader :expression
+end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,11 +1,24 @@
+# frozen_string_literal: true
+
 require 'calculator'
 
 RSpec.describe Calculator do
   describe '#solve' do
-    it 'handles empty input correctly'
-    it 'ingores whitespaces'
-    it 'can read integers'
+    it 'handles empty input correctly' do
+      expect(Calculator.new('').solve).to eq ''
+    end
+
+    it 'ingores whitespaces' do
+      expect(Calculator.new('   ').solve).to eq ''
+    end
+
+    it 'can read integers' do
+      expect(Calculator.new('100644').solve).to eq '100644'
+    end
+
     it 'can read floats'
-    it 'can add two numbers'
+    it 'can add two numbers' do
+      expect(Calculator.new('1 + 1 ').solve).to eq '2'
+    end
   end
 end
