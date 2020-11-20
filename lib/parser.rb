95ee4b36321c30e47f34f9db175dd51bdd686496
Leander Karp <leander.karp@invision.de>
2020-11-20 15:29:52 +0100

    Add integer parsing

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,5 +1,19 @@
 class Parser
   def self.parse(expression)
-    [expression]
+    tokens = []
+    current_token = ''
+    expression.split('').each_with_index do |character, index|
+      if '01'.include?(character)
+        current_token += character
+      else
+        tokens.append(character)
+      end
+
+      if index + 1 >= expression.length && !current_token.empty?
+        tokens.append(current_token)
+      end
+    end
+
+    tokens
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,10 +2,16 @@ require 'parser'
 
 RSpec.describe Parser do
   describe '#parse' do
-    it 'produces tokens' do
+    it 'generates whitespace tokens' do
       expected_tokens = [' ']
 
       expect(Parser.parse(' ')).to eq expected_tokens
+    end
+
+    it 'generates integers' do
+      expected_tokens = [' ', ' ', '1000']
+
+      expect(Parser.parse('  1000')).to eq expected_tokens
     end
   end
 end
